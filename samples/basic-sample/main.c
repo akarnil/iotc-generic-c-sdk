2e980dc5d4562cddf44302f915d2b7b8e4ffbf72
Alen Karnil <akarnil@witekio.com>
2024-03-28 12:03:50 +0000

    add json file support

--- samples/basic-sample/main.c
+++ samples/basic-sample/main.c
@@ -11,6 +11,12 @@
 #include "iotconnect.h"
 
 #include "app_config.h"
+#include "cJSON.h"
+
+
+#include <time.h>
+#include <sys/stat.h>
+
 
 // windows compatibility
 #if defined(_WIN32) || defined(_WIN64)
@@ -24,6 +30,23 @@
 #endif
 
 #define APP_VERSION "00.01.00"
+#define STRINGS_ARE_EQUAL 0
+#define FREE(x) if ((x)) { free(x); (x) = NULL; }
+
+typedef struct telemetry_attribute
+{
+    char* name;
+    int name_len;
+
+    char* path;
+    int path_len;
+
+    bool read_ascii;
+    time_t last_accessed;
+
+} telemetry_attribute_t;
+
+
 
 static void on_connection_status(IotConnectMqttStatus status) {
     // Add your own status handling
@@ -103,8 +126,42 @@ static void on_ota(IotclC2dEventData data) {
     iotcl_mqtt_send_ota_ack(ack_id, (success ? IOTCL_C2D_EVT_OTA_SUCCESS : IOTCL_C2D_EVT_OTA_DOWNLOAD_FAILED), message);
 }
 
-static void publish_telemetry() {
+static void publish_telemetry(int number_of_attributes, telemetry_attribute_t* telemetry) {
     IotclMessageHandle msg = iotcl_telemetry_create();
+
+    for (int i = 0; i < number_of_attributes; i++)
+    {
+        if (access(telemetry[i].path, F_OK) != 0 )
+        {
+            printf("failed to access input telemetry path - %s ; Skipping\n", telemetry[i].path);
+            continue;
+        }
+        struct stat file_stat;
+        if (stat(telemetry[i].path, &file_stat) == -1)
+        {
+            fprintf(stderr, "Error: %s\n", strerror(telemetry[i].path));
+            continue;
+        }
+
+        time_t modified_time = file_stat.st_mtime;
+        if (modified_time <= telemetry[i].last_accessed)
+        {
+            printf("telemetry not updated since last send - %s ; Skipping\n", telemetry[i].path);
+            continue;
+        }
+
+        FILE* fp = fopen(telemetry[i].path, "r");
+        char* buffer = NULL;
+        size_t len;
+        ssize_t bytes_read = getdelim( &buffer, &len, '\0', fp);
+        
+        fclose(fp);
+
+        iotcl_telemetry_set_string(msg, telemetry[i].name, APP_VERSION);
+
+        telemetry[i].last_accessed = modified_time;
+
+    }
 
     // STRING template field type
     iotcl_telemetry_set_string(msg, "version", APP_VERSION);
@@ -127,66 +184,288 @@ static void publish_telemetry() {
     iotcl_telemetry_destroy(msg);
 }
 
+
+static bool string_ends_with(const char * needle, const char* haystack)
+{
+    const char *str_end = haystack + strlen(haystack) -  strlen(needle);
+    return (strncmp(str_end, needle, strlen(needle) ) == 0);
+}
+
+int parse_raw_json_to_string(char* output, const char * const raw_json_str, char* key)
+{
+    const cJSON *value = NULL;
+    cJSON *json = cJSON_Parse(raw_json_str);
+    if (json == NULL)
+    {
+        const char *error_ptr = cJSON_GetErrorPtr();
+        if (error_ptr != NULL)
+        {
+            fprintf(stderr, "Error before: %s\n", error_ptr);
+        }
+        cJSON_Delete(json);
+        return EXIT_FAILURE; 
+    }
+
+    value = cJSON_GetObjectItemCaseSensitive(json, key);
+    if (cJSON_IsString(value) && (value->valuestring != NULL))
+    {
+        strncpy(output,value->valuestring, strlen(value->valuestring));
+        
+        cJSON_Delete(json);
+        return EXIT_SUCCESS;
+    }
+
+    cJSON_Delete(json);
+    return EXIT_FAILURE;
+}
+
+int parse_json_to_string(char* output, cJSON* json, char* key)
+{
+    const cJSON *value = NULL;
+    value = cJSON_GetObjectItemCaseSensitive(json, key);
+    if (cJSON_IsString(value) && (value->valuestring != NULL))
+    {
+        strncpy(output,value->valuestring, strlen(value->valuestring));
+        
+        return EXIT_SUCCESS;
+    }
+
+    return EXIT_FAILURE;
+}
+
+char device_id[256] = {};
+char company_id[256] = {};
+char environment[256] = {};
+char iotc_server_cert_path[4096] = {};
+char sdk_id[256] = {};
+
+char connection_type_str[256] = {};
+IotConnectConnectionType connection_type = 0; 
+
+char auth_type[256] = {};
+char commands_list_path[4096] = {};
+
+
+
+
 int main(int argc, char *argv[]) {
-    char *trust_store;
+
+    IotConnectClientConfig config;
+    iotconnect_sdk_init_config(&config);
+
+    telemetry_attribute_t* telemetry = NULL; 
+    
+
+    // need to pull the json path from argv
+    char json_path[4096] = "";
+
+    // if (argc == 2) {};
+
+    char json[] = "/home/akarnil/work/iotc-generic-c-sdk/samples/basic-sample/config.json";
+    strncpy(json_path,json,strlen(json));
+
+    if (access(json_path, F_OK) != 0)
+    {
+        printf("failed to access input json file - %s ; Aborting\n", json_path);
+        return EXIT_FAILURE;
+    }
+
+    FILE* fd = fopen(json_path, "r");
+    if (!fd)
+    {
+        printf("File failed to open - %s", json_path);
+        return EXIT_FAILURE;
+    }
+    fseek(fd, 0l, SEEK_END);
+    long file_len = ftell(fd);
+
+    if (file_len <= 0)
+    {
+        printf("failed calculating file length: %ld. Aborting\n", file_len);
+        return EXIT_FAILURE;
+    }
+    rewind(fd);
+
+    char* json_str = (char*)calloc(file_len+1, sizeof(char));
+    if (!json_str)
+    {
+        printf("failed to calloc. Aborting\n");
+        json_str = NULL;
+        return EXIT_FAILURE;
+    }
+    
+    for (int i = 0; i < file_len; i++)
+    {
+        json_str[i] = fgetc(fd);
+    }
+    fclose(fd);
+
+    cJSON* json_parser = NULL;
+    json_parser = cJSON_Parse(json_str);
+    if (!json_parser)
+    {
+        const char *error_ptr = cJSON_GetErrorPtr();
+        if (error_ptr != NULL)
+        {
+            fprintf(stderr, "Error before: %s\n", error_ptr);
+        }
+        cJSON_Delete(json_parser);
+        return EXIT_FAILURE;
+    }
+
+    int parsing_result = 0;
+    parsing_result += parse_raw_json_to_string(device_id,json_str,"duid");
+    parsing_result += parse_raw_json_to_string(company_id,json_str,"cpid");
+    parsing_result += parse_raw_json_to_string(environment,json_str,"env");
+    parsing_result += parse_raw_json_to_string(iotc_server_cert_path,json_str,"iotc_server_cert");
+    parsing_result += parse_raw_json_to_string(sdk_id,json_str,"sdk_id");
+    parsing_result += parse_raw_json_to_string(connection_type_str,json_str,"connection_type");
+
+    if (parsing_result != 0)
+    {
+        return EXIT_FAILURE;
+    }
+
+    cJSON* auth_parser = cJSON_GetObjectItemCaseSensitive(json_parser, "auth");
+    parsing_result += parse_json_to_string(auth_type,auth_parser,"auth_type");
+
+    if (strcmp(connection_type_str, "IOTC_CT_AWS") == STRINGS_ARE_EQUAL){
+        connection_type = IOTC_CT_AWS;
+    }
+    else if (strcmp(connection_type_str, "IOTC_CT_AZURE") == STRINGS_ARE_EQUAL){
+        connection_type = IOTC_CT_AZURE;
+    }
+
+    if (strcmp(auth_type, "IOTC_AT_X509") == STRINGS_ARE_EQUAL){
+        config.auth_info.type= IOTC_AT_X509;
+        char client_key[256] = {};
+        char client_cert[256] = {};
+
+        cJSON* params_parser = cJSON_GetObjectItemCaseSensitive(auth_parser, "params");
+        parsing_result += parse_json_to_string(client_key, params_parser, "client_key");
+        parsing_result += parse_json_to_string(client_cert, params_parser, "client_cert");
+        cJSON_free(params_parser);
+
+        config.auth_info.data.cert_info.device_cert = client_cert;
+        config.auth_info.data.cert_info.device_key = client_key;
+
+    } else if (strcmp(auth_type, "IOTC_AT_SYMMETRIC_KEY") == STRINGS_ARE_EQUAL){
+        config.auth_info.type= IOTC_AT_SYMMETRIC_KEY;
+    } else if (strcmp(auth_type, "IOTC_AT_TPM") == STRINGS_ARE_EQUAL) {
+        // config.auth_info.type= IOTC_AT_TPM;
+    } else if (strcmp(auth_type, "IOTC_AT_TOKEN") == STRINGS_ARE_EQUAL) {
+        // config.auth_info.type= IOTC_AT_TOKEN;
+    } else {
+        printf("unsupported auth type. Aborting\r\n");
+        return EXIT_FAILURE;
+    }
+
+    cJSON* device_parser = cJSON_GetObjectItemCaseSensitive(json_parser, "device");
+    parsing_result += parse_json_to_string(commands_list_path,device_parser,"commands_list_path");
+
+
+    cJSON* attribute = NULL;
+    cJSON* attributes_parser = cJSON_GetObjectItemCaseSensitive(device_parser, "attributes");
+    int number_of_attributes = 0;
+    cJSON_ArrayForEach(attribute, attributes_parser)
+    {
+        number_of_attributes++;
+    }
+    telemetry = (telemetry_attribute_t*)calloc(number_of_attributes, sizeof(telemetry_attribute_t)); 
+    telemetry_attribute_t* telem_ptr = telemetry;
+    cJSON_ArrayForEach(attribute, attributes_parser)
+    {
+        cJSON *name = cJSON_GetObjectItemCaseSensitive(attribute, "name");
+        telem_ptr->name_len = strlen(name->valuestring);
+        telem_ptr->name = calloc(telem_ptr->name_len, sizeof(char));
+        strncpy(telem_ptr->name, name->valuestring, telem_ptr->name_len);
+        
+        cJSON *path = cJSON_GetObjectItemCaseSensitive(attribute, "private_data");
+        telem_ptr->path_len = strlen(path->valuestring);
+        telem_ptr->path = calloc(telem_ptr->path_len, sizeof(char));
+        strncpy(telem_ptr->path, path->valuestring, telem_ptr->path_len);
+        
+        cJSON *read_type = cJSON_GetObjectItemCaseSensitive(attribute, "private_data_type");
+        if (strncmp(read_type->valuestring, "ascii", strlen("ascii")) == STRINGS_ARE_EQUAL)
+        {
+            telem_ptr->read_ascii = true;
+        }
+
+        telem_ptr++;
+    }
+
+    printf("%s\n", commands_list_path);
+    cJSON_free(attributes_parser);
+    cJSON_free(device_parser);
+    cJSON_free(auth_parser);
+    cJSON_free(json_parser);
+
+
+
+    printf("%s\n", device_id);
+    free(json_str);
+
+
+
+
+    // char *trust_store;
 
     (void) argc;
     (void) argv;
 
-#ifdef IOTCONNECT_MQTT_SERVER_CA_CERT
-    trust_store = IOTCONNECT_CA_CERT_PATH
-#else
-    if (IOTCONNECT_CONNECTION_TYPE == IOTC_CT_AWS) {
-        trust_store = IOTCONNECT_MQTT_SERVER_CA_CERT_DEFAULT_AWS;
-    } else {
-        trust_store = IOTCONNECT_MQTT_SERVER_CA_CERT_DEFAULT_AZURE;
-    }
-#endif
+// #ifdef IOTCONNECT_MQTT_SERVER_CA_CERT
+//     trust_store = IOTCONNECT_CA_CERT_PATH
+// #else
+//     if (IOTCONNECT_CONNECTION_TYPE == IOTC_CT_AWS) {
+//         trust_store = IOTCONNECT_MQTT_SERVER_CA_CERT_DEFAULT_AWS;
+//     } else {
+//         trust_store = IOTCONNECT_MQTT_SERVER_CA_CERT_DEFAULT_AZURE;
+//     }
+// #endif
 
-    if (access(trust_store, F_OK) != 0) {
-        printf("Unable to access the MQTT CA certificate. "
-               "Please change directory so that %s can be accessed from the application or update IOTCONNECT_CERT_PATH",
-               trust_store);
-        return -1;
-    }
+//     if (access(trust_store, F_OK) != 0) {
+//         printf("Unable to access the MQTT CA certificate. "
+//                "Please change directory so that %s can be accessed from the application or update IOTCONNECT_CERT_PATH",
+//                trust_store);
+//         return -1;
+//     }
 
-    if (IOTCONNECT_AUTH_TYPE == IOTC_AT_X509) {
-        if (access(IOTCONNECT_DEVICE_CERT, F_OK) != 0
-                ) {
-            printf("Unable to access device identity certificate. "
-                   "Please change directory so that %s can be accessed from the application or update IOTCONNECT_DEVICE_CERT",
-                   IOTCONNECT_DEVICE_CERT);
-            return -1;
-        }
-        if (access(IOTCONNECT_DEVICE_PRIVATE_KEY, F_OK) != 0
-                ) {
-            printf("Unable to access device identity private key. "
-                   "Please change directory so that %s can be accessed from the application or update IOTCONNECT_DEVICE_PRIVATE_KEY",
-                   IOTCONNECT_DEVICE_PRIVATE_KEY);
-            return -1;
-        }
-    }
+//     if (IOTCONNECT_AUTH_TYPE == IOTC_AT_X509) {
+//         if (access(IOTCONNECT_DEVICE_CERT, F_OK) != 0
+//                 ) {
+//             printf("Unable to access device identity certificate. "
+//                    "Please change directory so that %s can be accessed from the application or update IOTCONNECT_DEVICE_CERT",
+//                    IOTCONNECT_DEVICE_CERT);
+//             return -1;
+//         }
+//         if (access(IOTCONNECT_DEVICE_PRIVATE_KEY, F_OK) != 0
+//                 ) {
+//             printf("Unable to access device identity private key. "
+//                    "Please change directory so that %s can be accessed from the application or update IOTCONNECT_DEVICE_PRIVATE_KEY",
+//                    IOTCONNECT_DEVICE_PRIVATE_KEY);
+//             return -1;
+//         }
+//     }
 
-    IotConnectClientConfig config;
-    iotconnect_sdk_init_config(&config);
-    config.cpid = IOTCONNECT_CPID;
-    config.env = IOTCONNECT_ENV;
-    config.duid = IOTCONNECT_DUID;
-    config.connection_type = IOTCONNECT_CONNECTION_TYPE;
-    config.auth_info.type = IOTCONNECT_AUTH_TYPE;
-    config.auth_info.trust_store = trust_store;
+    config.cpid = company_id;
+    config.env = environment;
+    config.duid = device_id;
+    config.connection_type = connection_type;
+    // config.auth_info.type = IOTCONNECT_AUTH_TYPE;
+    config.auth_info.trust_store = iotc_server_cert_path;
     config.verbose = true;
 
-    if (config.auth_info.type == IOTC_AT_X509) {
-        config.auth_info.data.cert_info.device_cert = IOTCONNECT_DEVICE_CERT;
-        config.auth_info.data.cert_info.device_key = IOTCONNECT_DEVICE_PRIVATE_KEY;
-    } else if (config.auth_info.type == IOTC_AT_SYMMETRIC_KEY){
-        config.auth_info.data.symmetric_key = IOTCONNECT_SYMMETRIC_KEY;
-    } else {
-        // none of the above
-        printf("Unknown IotConnectAuthType\n");
-        return -1;
-    }
+
+    // if (config.auth_info.type == IOTC_AT_X509) {
+    //     config.auth_info.data.cert_info.device_cert = IOTCONNECT_DEVICE_CERT;
+    //     config.auth_info.data.cert_info.device_key = IOTCONNECT_DEVICE_PRIVATE_KEY;
+    // } else if (config.auth_info.type == IOTC_AT_SYMMETRIC_KEY){
+    //     config.auth_info.data.symmetric_key = IOTCONNECT_SYMMETRIC_KEY;
+    // } else {
+    //     // none of the above
+    //     printf("Unknown IotConnectAuthType\n");
+    //     return -1;
+    // }
 
 
     config.status_cb = on_connection_status;
@@ -203,24 +482,32 @@ int main(int argc, char *argv[]) {
         return ret;
     }
 
-    for (int j = 0; j < 10; j++) {
-        ret = iotconnect_sdk_connect();
-        if (0 != ret) {
-            printf("iotconnect_sdk_init() exited with error code %d\n", ret);
-            return ret;
-        }
-
-        // send 10 messages
-        for (int i = 0; iotconnect_sdk_is_connected() && i < 10; i++) {
-            publish_telemetry();
-            // repeat evey ~5 seconds
-            sleep(5);
-        }
-        iotconnect_sdk_disconnect();
+    ret = iotconnect_sdk_connect();
+    if (0 != ret) {
+        printf("iotconnect_sdk_init() exited with error code %d\n", ret);
+        return ret;
     }
 
+    while (iotconnect_sdk_is_connected())
+    {
+        publish_telemetry(number_of_attributes, telemetry);
+        sleep(5);
+    }
+    
+    iotconnect_sdk_disconnect();
     iotconnect_sdk_deinit();
 
     printf("Basic sample demo is complete. Exiting.\n");
+
+
+    // free attributes
+    for (int i = 0; i < number_of_attributes; i++)
+    {
+        printf("%s %s\n", telemetry[i].name, telemetry[i].path);
+        free(telemetry[i].name);
+        free(telemetry[i].path);
+    }
+    free(telemetry);
+
     return 0;
 }
